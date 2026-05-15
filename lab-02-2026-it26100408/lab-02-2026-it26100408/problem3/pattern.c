410ed4407f8ba59034e30fc77eb8c95990d2c2ff
it26100408 <IT26100408@my.sliit.lk>
2026-05-15 16:30:00 +0000

    Completed Problem 3: Diamond pattern

---
+++ lab-02-2026-it26100408/lab-02-2026-it26100408/problem3/pattern.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+      printf("    x    \n");
+      printf("   x x   \n");
+      printf("  x   x  \n");
+      printf("   x x   \n");
+      printf("  x   x  \n");
+      printf("   x x   \n");
+      printf("  x   x  \n");
+      printf("   x x   \n");
+      printf("    x    \n");
+      return 0;
+}
