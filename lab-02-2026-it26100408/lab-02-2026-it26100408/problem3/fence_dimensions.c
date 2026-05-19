48e6dff636b32729cbb135deca1c5a3f743acf83
it26100408 <it26100408@my.sliit.lk>
2026-05-19 11:53:14 +0000

    Refactor fence dimension calculation code

--- lab-02-2026-it26100408/lab-02-2026-it26100408/problem3/fence_dimensions.c
+++ lab-02-2026-it26100408/lab-02-2026-it26100408/problem3/fence_dimensions.c
@@ -2,17 +2,14 @@
 
       int main(){
               float perimeter, length,width;
-
-              // Get input from user
+            
               printf("Enter the perimeter of the fence: ");
               scanf("%f", &perimeter);
-
-              // Calculate dimensions
+            
               //Formula: P = 2(L + 0.75L) -> P = 3.5L
               length = perimeter / 3.5;
               width = 0.75 * length;
-
-              // Display results
+            
               printf("Length of the fence: %.2f\n", length);
               printf("Width of the fence: %.2f\n", width);
  
